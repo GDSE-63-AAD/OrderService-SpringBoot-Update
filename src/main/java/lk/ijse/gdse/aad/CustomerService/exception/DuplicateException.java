befd4c54a7b9a621f3b578cf5f0f2caf3525aa26
ThanuraSilva <thanurasilva1989@gmail.com>
2023-09-16 21:18:56 +0530

    Custom exception create

---
+++ src/main/java/lk/ijse/gdse/aad/CustomerService/exception/DuplicateException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad.CustomerService.exception;
+
+public class DuplicateException extends RuntimeException{
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
